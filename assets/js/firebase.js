19d7ec06101e13826036c0319227a978f0743507
Noelia <masgonoelia21@gmail.com>
2025-02-14 08:04:31 -0500

    navbar

---
+++ assets/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxOVGBYquXye8dyDoPvL2WosF7PsmwCp4",
+  authDomain: "pagina-ebiz.firebaseapp.com",
+  projectId: "pagina-ebiz",
+  storageBucket: "pagina-ebiz.firebasestorage.app",
+  messagingSenderId: "927403094325",
+  appId: "1:927403094325:web:9e6fa51ff3f84670a61c7c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
